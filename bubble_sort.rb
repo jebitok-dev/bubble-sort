a283e404f874b9cbe018cff827437bedf3f1788c
chrystalme <afam.ifeanyi@gmail.com>
2021-01-13 17:11:52 +0200

    Add bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,17 +2,45 @@ def bubble_sort(arr)
     len = arr.length
     arr if len <= 1
 
-    arr_new = true 
-    while arr_new
-        arr_new = false
+    is_sorted = true 
+    while is_sorted
+        is_sorted = false
         (0...len - 1).each do |i|
             if arr[i] > arr[i + 1] 
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                arr_new = true
+                is_sorted = true
             end
         end
     end
     arr
 end
-p bubble_sort([11, 5])
+to_sort = [11, 5]
 
+def bubble_sort_by(arr)
+    include Comparable
+    len = arr.length
+    arr if len <= 1
+    
+    have_sorted = true
+    while have_sorted
+        have_sorted = false
+        (0...len - 1).each do |i|
+
+            num = yield(arr[i], arr[i + 1])
+            next unless num >= 1  
+            
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                have_sorted = true
+            end
+        end
+    end
+    arr
+end
+
+bubble_sort_by(["hi","hello", "hey"]) do |left, right|
+    left.length - right.length
+end
+p bubble_sort_by(to_sort)
+# sort_this = [2,0,4,3,7,14,1,1]
+# p bubble_sort_by([2,0,4,3,7,14,1,1])
